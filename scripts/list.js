11410f02388438dd2724474a30b40f7644afc4af
Nikita <nikita.ivanov@1win.pro>
2025-01-30 13:03:44 +0500

    fix: fix bugs

--- scripts/list.js
+++ scripts/list.js
@@ -3,8 +3,6 @@ import { app } from "./app.js";
 function renderUserList() {
   const userList = JSON.parse(localStorage.getItem("users"));
 
-  console.log(userList);
-
   for (const user of userList) {
     const userLineHTML = app.templates.userTablesLine(user);
     console.log(userLineHTML);
